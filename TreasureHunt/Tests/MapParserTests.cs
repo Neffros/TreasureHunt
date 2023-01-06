ef49c8e713cd55e8cafa2bb10eceeeb5c49cf7da
lior <diler.lior@gmail.com>
2023-01-06 07:45:09 +0100

    refactor (parser): Renamed MapParserTests tests

--- TreasureHunt/Tests/MapParserTests.cs
+++ TreasureHunt/Tests/MapParserTests.cs
@@ -1,7 +1,7 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using TreasureHunt.Models;
 using TreasureHunt.Exceptions;
+using TreasureHunt.Models;
 
 namespace TreasureHunt.Tests;
 
@@ -21,9 +21,9 @@ public class MapParserTests
 
     [Test]
     [TestCase("C - 3  - 5", 3 , 5)]
-    [TestCase("C - 7 - 2", 7, 2)]
-    [TestCase("C - 8 - 8 ", 8, 8)]
-    public void ShouldCreateEmptyMap_WithDimension_ReturnsSameMap(string line, int expectedWidth, int expectedHeight)
+    [TestCase("C - 7 - 2", 7,2)]
+    [TestCase("C - 8 - 8 ", 8,8)]
+    public void CreateMap_WithDimension_ReturnsParsedMap(string line, int expectedWidth, int expectedHeight)
     {
         Map expected = new (new Dimension(expectedWidth, expectedHeight), new List<Mountain>(), new List<Treasure>(),
             new List<Adventurer>());
@@ -36,7 +36,7 @@ public class MapParserTests
     
     [Test]
     [TestCase(arg: new[]{""})]
-    public void ShouldNotCreateMap_WhenNoLines_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_WithNoLines_ThrowsInitializationMapException(string[] lines)
     {
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
@@ -45,7 +45,7 @@ public class MapParserTests
     [Test]
     [TestCase(new[]{"# {C comme Carte} - {Nb. de case en largeur} - {Nb. de case en hauteur}", "C - 5 - 4"}, 5, 4)]
     [TestCase(new[]{"C - 5 - 4", "# {M comme Montagne} - {Axe horizontal} - {Axe vertical}"}, 5, 4)]
-    public void ShouldCreateMap_WithComment_IgnoresComments(string[] lines, int expectedWidth, int expectedHeight)
+    public void CreateMap_WithComment_IgnoresComments(string[] lines, int expectedWidth, int expectedHeight)
     {
         Map expected = new Map(new Dimension(expectedWidth, expectedHeight), new List<Mountain>(), new List<Treasure>(),
             new List<Adventurer>());
@@ -80,7 +80,7 @@ public class MapParserTests
 
     [Test]
     [TestCaseSource(nameof(MountainTestData))]
-    public void ShouldCreateMap_WithMountains_ReturnsSameMap(string[] lines, List<Mountain> expectedMountains)
+    public void CreateMap_WithMountains_ReturnsParsedMap(string[] lines, List<Mountain> expectedMountains)
     {
 
         Map expected = new Map(TestDataUtils.SampleDimension, expectedMountains, new List<Treasure>(),
@@ -95,7 +95,7 @@ public class MapParserTests
     [Test]
     [TestCase(arg: new[]{ "C - 5 - 4", "M - 1 - 0 - 2"})]
     [TestCase(arg: new[]{"C - 5 - 4", "M - 1"})]
-    public void ShouldNotCreateMap_WithMountainsWhenWrongFormat_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_WithMountainsWhenWrongFormat_ThrowsInitializationMapException(string[] lines)
     {
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
@@ -129,7 +129,7 @@ public class MapParserTests
     
     [Test]
     [TestCaseSource(nameof(TreasureTestData))]
-    public void ShouldCreateMap_WithTreasures_ReturnsSameMap(string[] lines, List<Treasure> expectedTreasures)
+    public void CreateMap_WithTreasures_ReturnsParsedMap(string[] lines, List<Treasure> expectedTreasures)
     {
         Map expected = new Map(TestDataUtils.SampleDimension, new List<Mountain>(), expectedTreasures,
             new List<Adventurer>());
@@ -142,7 +142,7 @@ public class MapParserTests
     [Test]
     [TestCase(arg: new[]{ "C - 5 - 4", "T - 1 - 0 - 2 - 4"})]
     [TestCase(arg: new[]{"C - 5 - 4", "T - X - 1"})]
-    public void ShouldNotCreateMap_WithTreasuresWhenWrongFormat_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_WithTreasuresWhenWrongFormat_ThrowsInitializationMapException(string[] lines)
     {
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
@@ -182,7 +182,7 @@ public class MapParserTests
     
     [Test]
     [TestCaseSource(nameof(AdventurerTestData))]
-    public void ShouldCreateMap_WithAdventurer_ReturnsSameMap(string [] lines, List<Adventurer> expectedAdventurers)
+    public void CreateMap_WithAdventurer_ReturnsParsedMap(string [] lines, List<Adventurer> expectedAdventurers)
     {
         Map expected = new (TestDataUtils.SampleDimension, new List<Mountain>() , new List<Treasure>(),
             expectedAdventurers);
@@ -194,9 +194,8 @@ public class MapParserTests
     [Test]
     [TestCase(arg: new[]{ "C - 5 - 4", "A - 1 - 0 - 2"})]
     [TestCase(arg: new[]{"C - 5 - 4", "A - X - 1"})]
-    public void ShouldNotCreateMap_WithAdventurersWhenWrongFormat_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_WithAdventurersWhenWrongFormat_ThrowsInitializationMapException(string[] lines)
     {
-        
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
     }
@@ -207,7 +206,7 @@ public class MapParserTests
     [Test]
     [TestCase(arg: new[]{"C - 5 - 4", "M - 5 - 10"})]
     [TestCase(arg: new[]{"C - 5 - 4", "T - -5 - 4"})]
-    public void ShouldNotCreateMap_WithOutOfBoundsEntities_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_WithOutOfBoundsEntities_ThrowsInitializationMapException(string[] lines)
     {
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
@@ -216,7 +215,7 @@ public class MapParserTests
     [Test]
     [TestCase(arg: new[]{"C - 5 - 4", "A - Lara - 1 - 1 - E - P"})]
     [TestCase(arg: new[]{"C - 5 - 4",  "A - Lara - 1 - 1 - E - LU"})]
-    public void ShouldNotCreateMap_WithAdventurersWhenInvalidInstructions_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_WithAdventurersWhenInvalidInstructions_ThrowsInitializationMapException(string[] lines)
     {
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
@@ -225,14 +224,14 @@ public class MapParserTests
     [Test]
     [TestCase(arg: new[]{"C - 5 - 4", "A - Sophie - 1 - 1 - E - A", "A - Lara - 1 - 1 - E - A"})]
     [TestCase(arg: new[]{"C - 5 - 4", "A - Lior - 5 - 4 - E - A", " Sophie - 4 - 4 - E - A", "A - Lara - 4 - 4 - E - A"})]
-    public void ShouldNotCreateMap_MapWithEntitiesOverlapping_ThrowsInitializationMapException(string[] lines)
+    public void CreateMap_MapWithEntitiesOverlapping_ThrowsInitializationMapException(string[] lines)
     {
         Action createMap = () => _mapParser.CreateMap(lines);
         createMap.Should().Throw<InitializationMapException>();
     }
     
     [Test]
-    public void ShouldCreateMap_WithAllEntities_ReturnsSameMap()
+    public void CreateMap_WithAllEntities_ReturnsParsedMap()
     {
         string[] lines =
         {
